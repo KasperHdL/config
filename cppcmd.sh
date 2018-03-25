ffa6dc1ea8967bfe2dfee946ecff314baee72968
Kasper Honnens de Lichtenberg <kasper@honnens.dk>
2018-03-25 13:41:47 +0200

    cppcmd now accepts 2 or 3 arguments allowing for cppcmd mv from to

--- cppcmd.sh
+++ cppcmd.sh
@@ -3,6 +3,13 @@ set -e
 
 ROOT="$(git rev-parse --show-toplevel)"
 
-$1 ${ROOT}/include/$2.hpp
-$1 ${ROOT}/src/$2.cpp
+if (( $# <= 1 )); then
+    echo "Illegal number of arguments"
+elif (( $# == 2 )); then
+    $1 ${ROOT}/include/$2.hpp
+    $1 ${ROOT}/src/$2.cpp
+elif (( $# == 3 )); then
+    $1 ${ROOT}/include/$2.hpp ${ROOT}/include/$3.hpp
+    $1 ${ROOT}/src/$2.cpp ${ROOT}/src/$3.cpp
+fi
 
